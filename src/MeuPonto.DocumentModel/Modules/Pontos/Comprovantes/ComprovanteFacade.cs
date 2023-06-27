30dcd2eeb879d03f713dfb0453df61a04733a3aa
Marcelo Dias <mardsystems@gmail.com>
2023-06-27 12:22:08 -0300

    fix(comprovantes): ajusta detalhes

--- src/MeuPonto.DocumentModel/Modules/Pontos/Comprovantes/ComprovanteFacade.cs
+++ src/MeuPonto.DocumentModel/Modules/Pontos/Comprovantes/ComprovanteFacade.cs
@@ -2,15 +2,15 @@
 
 public static class ComprovanteFacade
 {
-    public static void ComprovaPonto(this Comprovante comprovante, Pontos.Ponto ponto)
+    public static void ComprovaPonto(this Comprovante comprovante, Ponto ponto)
     {
         comprovante.Ponto = new PontoRef
         {
-            PerfilId = ponto?.PerfilId,
-            DataHora = ponto?.DataHora,
-            Perfil = ponto?.Perfil,
-            MomentoId = ponto?.MomentoId,
-            PausaId = ponto?.PausaId
+            PerfilId = ponto.PerfilId,
+            DataHora = ponto.DataHora,
+            Perfil = ponto.Perfil,
+            MomentoId = ponto.MomentoId,
+            PausaId = ponto.PausaId
         };
 
         comprovante.PontoId = ponto.Id;
