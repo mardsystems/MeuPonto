0cf3b3e9da585572dc848a42914ad99e5596b5cf
Marcelo Dias <mardsystems@gmail.com>
2023-07-27 13:23:44 -0300

    refact: formata horas com mais de 2 dígitos

---
+++ src/System.Extensions/TimeSpanFormatter.cs
@@ -0,0 +1,43 @@
+﻿namespace System;
+
+public class TimeSpanFormatter : IFormatProvider, ICustomFormatter
+{
+    public object? GetFormat(Type? formatType)
+    {
+        if (formatType == typeof(ICustomFormatter))
+            return this;
+        else
+            return null;
+    }
+
+    public string Format(string? format, object? arg, IFormatProvider? formatProvider)
+    {
+        if (!formatProvider.Equals(this)) return null;
+
+        string output;
+
+        if (format == "hhh\\:mm")
+        {
+            if (arg is TimeSpan timeSpan)
+            {
+                int totalHours = (Math.Abs(timeSpan.Days) * 24) + Math.Abs(timeSpan.Hours);
+
+                output = $"{totalHours:00}:{Math.Abs(timeSpan.Minutes):00}";
+            }
+            else if (arg == null)
+            {
+                output = $"00:00";
+            }
+            else
+            {
+                output = String.Format(format, arg);
+            }
+        }
+        else
+        {
+            output = String.Format(format, arg);
+        }
+
+        return output;
+    }
+}
