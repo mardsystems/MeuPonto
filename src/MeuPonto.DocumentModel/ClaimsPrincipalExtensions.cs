1303c0d2a7f79fbc5ead1049b968d37de1566334
Marcelo Dias <mardsystems@hotmail.com>
2023-09-08 12:17:11 -0300

    task: adiciona extensão GetUserId ao User

--- src/MeuPonto.DocumentModel/ClaimsPrincipalExtensions.cs
+++ src/MeuPonto.DocumentModel/ClaimsPrincipalExtensions.cs
@@ -6,12 +6,19 @@ public static class ClaimsPrincipalExtensions
 {
     public static TransactionContext CreateTransaction(this ClaimsPrincipal user)
     {
-        var nameIdentifier = user.FindFirst(ClaimTypes.NameIdentifier);
-
-        var userId = Guid.Parse(nameIdentifier.Value);
+        var userId = user.GetUserId();
 
         var transaction = new TransactionContext(userId);
 
         return transaction;
+    }
+
+    public static Guid GetUserId(this ClaimsPrincipal user)
+    {
+        var nameIdentifier = user.FindFirst(ClaimTypes.NameIdentifier);
+
+        var userId = Guid.Parse(nameIdentifier.Value);
+
+        return userId;
     }
 }
