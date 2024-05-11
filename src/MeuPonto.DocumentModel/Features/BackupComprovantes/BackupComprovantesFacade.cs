4f014cc534c35a2f61e9be608ed861e74cbf9d30
Marcelo Dias <mardsystems@gmail.com>
2024-05-11 06:43:36 -0300

    task: adiciona "Backup*" no versionamento do código fonte

---
+++ src/MeuPonto.DocumentModel/Features/BackupComprovantes/BackupComprovantesFacade.cs
@@ -0,0 +1,44 @@
+﻿using System.Transactions;
+using MeuPonto.Models.Pontos;
+
+namespace MeuPonto.Features.BackupComprovantes;
+
+public static class BackupComprovantesFacade
+{
+    public static Comprovante CriaComprovante(TransactionContext transaction, Guid? id = null)
+    {
+        var comprovante = new Comprovante
+        {
+            Id = id ?? Guid.NewGuid(),
+            UserId = transaction.UserId,
+            PartitionKey = $"{transaction.UserId}",
+            CreationDate = transaction.DateTime
+        };
+
+        return comprovante;
+    }
+
+    public static void RecontextualizaComprovante(this Comprovante comprovante, TransactionContext transaction, Guid? id = null)
+    {
+        comprovante.Id ??= id ?? Guid.NewGuid();
+        comprovante.UserId = transaction.UserId;
+        //comprovante.PartitionKey = $"{transaction.UserId}|{comprovante.Ponto.DataHora:yyyy}";
+        comprovante.CreationDate ??= transaction.DateTime;
+    }
+
+    public static void ComprovaPonto(this Comprovante comprovante, Ponto ponto)
+    {
+        comprovante.Ponto = new PontoRef
+        {
+            ContratoId = ponto.ContratoId,
+            DataHora = ponto.DataHora,
+            Contrato = ponto.Contrato,
+            MomentoId = ponto.MomentoId,
+            PausaId = ponto.PausaId
+        };
+
+        comprovante.PartitionKey = $"{comprovante.UserId}|{comprovante.Ponto.DataHora:yyyy}";
+
+        comprovante.PontoId = ponto.Id;
+    }
+}
